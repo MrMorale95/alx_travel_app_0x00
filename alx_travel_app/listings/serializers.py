9a55b4882697252298a0b4280f170a6c7e926b03
Franklin Zyambo <frankzyambo95@gmail.com>
2025-07-21 20:00:44 +0200

    commit

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -6,18 +6,32 @@ class ListingSerializer(serializers.ModelSerializer):
     """
     Serializer for Listing model.
     """
-    
+
     class Meta:
         model = Listing
-        fields = ["id", 'name', 'description', 'location', 'price_per_night', 'created_at', 'updated_at']
+        fields = [
+            "id",
+            "name",
+            "description",
+            "location",
+            "price_per_night",
+            "created_at",
+            "updated_at",
+        ]
 
-    
 
 class BookingSerializer(serializers.ModelSerializer):
     """
     Serializer for Booking model.
     """
-    
+
     class Meta:
         model = Booking
-        fields = ['id', 'listing', 'start_date', 'end_date', 'status', 'created_at'] 
+        fields = [
+            "id",
+            "listing",
+            "start_date",
+            "end_date",
+            "status",
+            "created_at",
+        ]
